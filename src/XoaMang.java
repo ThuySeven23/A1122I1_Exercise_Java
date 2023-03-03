33b1acdfbb923e0a83edf357183e3897104c6c7c
ht304 <96908813+ThuySeven23@users.noreply.github.com>
2023-03-03 13:01:17 +0700

    Update XoaMang.java

--- src/XoaMang.java
+++ src/XoaMang.java
@@ -1,5 +1,21 @@
 public class XoaMang{
-    public static void main(String[] args) {
-        
+ public static void main(String[] args) {
+        int arr[] = new int[]{1, 2, 2, 4, 5, 6};
+        System.out.println("Mảng ban đầu : " + Arrays.toString(arr));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập phần tử cần xóa");
+        int x = sc.nextInt();
+        int c = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != x) {
+                arr[c] = arr[i];
+                c++;
+            }
+        }
+        int n = arr.length;
+        n = c;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
