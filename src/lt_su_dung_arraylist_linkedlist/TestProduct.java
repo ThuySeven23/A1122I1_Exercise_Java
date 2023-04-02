3f4b046cad75a316d11887b7980f4d203bb5a2d0
ht304 <96908813+ThuySeven23@users.noreply.github.com>
2023-04-02 21:08:51 +0700

    Add files via upload

---
+++ src/lt_su_dung_arraylist_linkedlist/TestProduct.java
@@ -0,0 +1,23 @@
+package lt_su_dung_arraylist_linkedlist;
+
+public class TestProduct {
+    public static void main(String[] args) {
+        ProductManager prM = new ProductManager();
+        Product  ao = new Product("áo","1",10);
+        Product  quan = new Product("quần","2",12);
+        Product  giay = new Product("giày","3",9);
+        Product  vay = new Product("váy","4",6);
+        Product vay2 = new Product("váy","5",11);
+
+        prM.addProduct(ao);
+        prM.addProduct(quan);
+        prM.addProduct(giay);
+        prM.addProduct(vay);
+        prM.addProduct(vay2);
+        prM.display();
+        System.out.println("");
+        prM.findProduct();
+        prM.sortProduct();
+        prM.display();
+    }
+}
