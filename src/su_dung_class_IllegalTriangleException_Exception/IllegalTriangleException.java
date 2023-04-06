dfa34c0492de2cd7ba1197c6f61929aee70dafc1
ht304 <96908813+ThuySeven23@users.noreply.github.com>
2023-04-06 11:38:17 +0700

    Add files via upload

---
+++ src/su_dung_class_IllegalTriangleException_Exception/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package su_dung_class_IllegalTriangleException_Exception;
+
+public class IllegalTriangleException extends Exception{
+    IllegalTriangleException(String s){
+        super(s);
+    }
+}
---
+++ src/su_dung_class_IllegalTriangleException_Exception/Triangle.java
@@ -0,0 +1,27 @@
+package su_dung_class_IllegalTriangleException_Exception;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public Triangle(int a , int b , int c) throws IllegalTriangleException {
+        if (a + b > c || b + c > a || c + a > b)
+            System.out.println("Triangle follows the rule");
+        else
+            System.out.println("Triangle doesn't follow the rule");
+    }
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        try{
+            int a,b,c;
+            System.out.print("Enter a: "); a = sc.nextInt();
+            System.out.print("Enter b: "); b = sc.nextInt();
+            System.out.print("Enter c: "); c = sc.nextInt();
+            System.out.println("Triangle(" + a + "," + b + "," + c + ")");
+            Triangle triangle = new Triangle(a,b,c);
+        }catch (Exception e){
+            System.err.println("Exception occured: " + e);
+        }
+    }
+
+}
